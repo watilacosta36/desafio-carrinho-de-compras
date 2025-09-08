e05a398feb6efb3ec85dfadbaa7e7e85c5270f03
Watila Costa <watilafreire26@gmail.com>
2025-09-07 23:33:44 -0300

    added endpoints to controllers carts and products with versions

--- app/controllers/api/v1/carts_controller.rb
+++ app/controllers/api/v1/carts_controller.rb
@@ -1,13 +1,44 @@
-class CartsController < ApplicationController
-  def create
-    @cart = Cart.create!(cart_params)
+module Api
+  module V1
+    class CartsController < ApplicationController
+      def create
+        result = ::CreateCartOrganizer.call(permitted_params, session)
+        session[:cart_id] = result.session[:cart_id]
 
-    render json: @cart, status: :created, location: @cart
-  end
+        render_json(result, :created)
+      end
 
-  private
+      def show
+        result = ::ListCartItemsOrganizer.call(session)
 
-  def cart_params
-    params.require(:cart).permit(:id)
+        render_json(result, :ok)
+      end
+
+      def add_item
+        result = ::AddCartItemOrganizer.call(permitted_params, session)
+
+        render_json(result, :ok)
+      end
+
+      def remove_item
+        result = ::RemoveCartItemOrganizer.call(permitted_params, session)
+
+        render_json(result, :ok)
+      end
+
+      private
+
+      def render_json(result, success_status)
+        if result.failure?
+          render json: { message: result.message }, status: result.error_code || :unprocessable_entity
+        else
+          render json: result.payload, status: success_status
+        end
+      end
+
+      def permitted_params
+        params.permit(:product_id, :quantity)
+      end
+    end
   end
 end
