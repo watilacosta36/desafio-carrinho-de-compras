4c1bb5b54fe063da1249ad25b2e0b82160095bcc
Watila Costa <watilafreire26@gmail.com>
2025-09-07 23:31:48 -0300

    validations, relations and some methods to models

---
+++ app/models/cart_item.rb
@@ -0,0 +1,7 @@
+class CartItem < ApplicationRecord
+  belongs_to :cart
+  belongs_to :product
+
+  validates :quantity, numericality: { only_integer: true, greater_than: 0 }
+  validates :total_price, numericality: { greater_than_or_equal_to: 0 }
+end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ApplicationRecord
+  has_many :cart_items, dependent: :destroy
+  has_many :carts, through: :cart_items
+
   validates_presence_of :name, :price
   validates_numericality_of :price, greater_than_or_equal_to: 0
 end
